f8908ee06b907704025ca66ed7a8aea98a30ceb2
Alex Jackson <contact@ajaxbits.com>
2023-12-03 18:13:47 -0600

    WIP

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,5 +1,6 @@
-use rayon::prelude::*;
+use rayon::{collections::linked_list, prelude::*};
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Color {
     Red,
     Blue,
@@ -20,24 +21,63 @@ fn get_symbol_indices(row: &str) -> Option<Vec<i32>> {
     }
 }
 
-fn color(row: &str) -> Option<Vec<(i32, i32)>> {
-    let indices = get_symbol_indices(&row);
-    indices.map(|indices: Vec<i32>| {
-        indices
-            .into_iter()
-            .map(|i| {
-                let max_len = row.len() as i32;
-                let poss_low = i - 1;
-                let poss_high = i + 1;
-                match (poss_low >= 0, poss_high <= max_len) {
-                    (true, true) => (poss_low, poss_high),
-                    (false, true) => (i, poss_high),
-                    (true, false) => (poss_low, i),
-                    (false, false) => (i, i),
-                }
-            })
-            .collect::<Vec<(i32, i32)>>()
-    })
+fn color_row(row: &str) -> Vec<Color> {
+    let len = row.len();
+    let indices: Option<Vec<i32>> = get_symbol_indices(&row);
+    let mut colors: Vec<Color> = vec![Color::Red; len];
+    if let Some(indices) = indices {
+        for i in indices {
+            colors[i as usize] = Color::Blue;
+            if i > 0 {
+                colors[(i - 1) as usize] = Color::Blue;
+            }
+            if i + 1 < len as i32 {
+                colors[(i + 1) as usize] = Color::Blue;
+            }
+        }
+    }
+    colors
+}
+
+fn color_plane(schematic: &str) -> Vec<Vec<Color>> {
+    let rows: Vec<&str> = schematic.lines().collect();
+    let colored_rows: Vec<Vec<Color>> = rows.iter().map(|row| color_row(row)).collect();
+    let line_len = rows[0].len();
+
+    let prepared_rows: Vec<Vec<char>> = rows
+        .into_iter()
+        .map(|row: &str| row.chars().collect::<Vec<char>>())
+        .zip(colored_rows.into_iter())
+        .map(|(row, colors): (Vec<char>, Vec<Color>)| {
+            row.into_iter()
+                .zip(colors.into_iter())
+                .map(|(c, color): (char, Color)| -> char {
+                    if color == Color::Red {
+                        c
+                    } else {
+                        '*'
+                    }
+                })
+                .collect::<Vec<char>>()
+        })
+        .collect();
+
+    let mut cols: Vec<String> = Vec::new();
+    for i in 1..line_len {
+        let mut col: Vec<char> = Vec::new();
+        for row in prepared_rows.iter() {
+            col.push(row[i]);
+        }
+        let col: String = col.iter().collect();
+        cols.push(col)
+    }
+    let colored_cols: Vec<Vec<Color>> = cols.iter().map(|s| color_row(s)).collect();
+
+    todo!()
+}
+
+fn find_numbers(row: &str) -> Option<Vec<i32>> {
+    todo!()
 }
 
 pub fn problem_1(input: &str) -> i32 {
@@ -65,14 +105,20 @@ mod tests {
 
     #[test]
     fn test_color() {
+        use Color::{Blue, Red};
+
         let example =
             fs::read_to_string("./data/examples/03/problem1.txt").expect("error loading input");
         let example: Vec<&str> = example.lines().collect();
-        assert_eq!(color(example[0]), None);
-        assert_eq!(color(example[1]), Some(vec![(2, 4)]));
-        assert_eq!(color(example[8]), Some(vec![(2, 6)]));
-        assert_eq!(color(example[10]), Some(vec![(2, 6), (8, 10)]));
-        assert_eq!(color(example[11]), Some(vec![(0, 1), (2, 6), (8, 10)]));
+        assert_eq!(color_row(example[0]), vec![Red; 10]);
+        assert_eq!(
+            color_row(example[1]),
+            vec![Red, Red, Blue, Blue, Blue, Red, Red, Red, Red, Red]
+        );
+        assert_eq!(
+            color_row(example[10]),
+            vec![Red, Red, Blue, Blue, Blue, Blue, Blue, Blue, Blue, Blue]
+        );
     }
 
     // #[test]
